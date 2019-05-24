4a924223f085de9c75beae3a112c51183a4de59d
Evgeny Fenko <fenko.ievgenii@gmail.com>
2019-05-24 19:41:58 +0300

    lesson 99 time 2:53 added side menu

--- public/manage_content.php
+++ public/manage_content.php
@@ -13,15 +13,47 @@
 ?>
 
 <?php require_once("../includes/functions.php"); ?>
+
+<?php
+
+    $query = "SELECT *";
+    $query .= "FROM subjects ";
+    $query .= "WHERE visible = 1 ";
+    $query .= "ORDER by position ASC";
+    $result = mysqli_query($connection, $query);
+
+    if(!$result){
+        die("Database query failed.");
+    }
+?>
 <?php include("../includes/layouts/header.php"); ?>
 
 <div id="main">
     <div id="navigation">
-        &nbsp;
+        <ul>
+            <?php
+
+                while ($subject = mysqli_fetch_assoc($result)){
+
+            ?>
+            <li><?php echo $subject["menu_name"] . "(" . $subject["id"] . ")"; ?></li>
+            <?php
+                }
+            ?>
+        </ul>
     </div>
     <div id="page">
         <h2>Manage Content</h2>
+
     </div>
 </div>
+<?php
+
+mysqli_free_result($result);
+?>
 
 <?php include("../includes/layouts/footer.php"); ?>
+<?php
+
+    mysqli_close($connection);
+?>
