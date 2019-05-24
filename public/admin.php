996683261988a580c01e0c266afabf8cf63e1da4
Evgeny Fenko <fenko.ievgenii@gmail.com>
2019-05-24 17:37:05 +0300

    end of lesson 98

--- public/admin.php
+++ public/admin.php
@@ -1,28 +1,19 @@
-<!DOCTYPE html PUBLIC>
+<?php require_once("../includes/functions.php"); ?>
+<?php include("../includes/layouts/header.php"); ?>
 
-<html lang="en">
-    <head>
-        <title>untiled</title>
-        <link href="stylesheets/public.css" media="all" rel="stylesheet" type="text/css"/>
-    </head>
-    <body>
-        <div id="header">
-            <h1>Widget Corp</h1>
-        </div>
-        <div id="main">
-            <div id="navigation">
-                &nbsp;
-            </div>
-            <div id="page">
-                <h2>Admin menu</h2>
-                <p>Welcome to the admin area.</p>
-                <ul>
-                    <li><a href="manage_content.php">Manage Website Content</a> </li>
-                    <li><a href="manage_admins.php">Manage Admin Users</a> </li>
-                    <li><a href="logout.php">Logout</a> </li>
-                </ul>
-            </div>
-        </div>
-        <div id="footer">Copyright 20xx, Widget Corp</div>
-    </body>
-</html>
+<div id="main">
+    <div id="navigation">
+        &nbsp;
+    </div>
+    <div id="page">
+        <h2>Admin menu</h2>
+        <p>Welcome to the admin area.</p>
+        <ul>
+            <li><a href="manage_content.php">Manage Website Content</a> </li>
+            <li><a href="manage_admins.php">Manage Admin Users</a> </li>
+            <li><a href="logout.php">Logout</a> </li>
+        </ul>
+    </div>
+</div>
+
+<?php include("../includes/layouts/footer.php"); ?>
