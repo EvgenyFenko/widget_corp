7f00358ddef32103f85abdc4d9867cd01a4364af
Evgeny Fenko <fenko.ievgenii@gmail.com>
2019-05-24 18:19:41 +0300

    lesson 99 time 1:30

--- public/manage_content.php
+++ public/manage_content.php
@@ -1,3 +1,17 @@
+<?php
+
+    $dbhost = "localhost";
+    $dbuser = "root";
+    $dbpass = "";
+    $dbname = "widget_corp";
+    $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+    if(mysqli_connect_errno()){
+        die("Database connection failed: ".mysqli_connect_error()." (".mysqli_connect_errno().")"
+        );
+    }
+?>
+
 <?php require_once("../includes/functions.php"); ?>
 <?php include("../includes/layouts/header.php"); ?>
 
