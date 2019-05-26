32ee08775f1672b5cc0ced57ec8b4bdc101ab0fc
EvgenyFenko <fenko.ievgenii@gmail.com>
2019-05-26 23:30:24 +0300

    end of lesson 104 does not work

--- includes/functions.php
+++ includes/functions.php
@@ -20,14 +20,36 @@
 
     function find_pages_for_subject($subject_id){
         global $connection;
+
+        $safe_subject_id = mysqli_real_escape_string($connection, $subject_id);
+
         $query = "SELECT *";
         $query .= "FROM pages ";
         $query .= "WHERE visible = 1 ";
-        $query .= "AND subject_id = {$subject_id} ";
+        $query .= "AND subject_id = {$safe_subject_id} ";
         $query .= "ORDER by position ASC";
         $page_set = mysqli_query($connection, $query);
         confirm_query($page_set);
         return $page_set;
+    }
+
+
+    function find_subject_by_id($subject_id){
+        global $connection;
+
+        $safe_subject_id = mysqli_real_escape_string($connection, $subject_id);
+
+        $query = "SELECT * ";
+        $query .= "FROM subjects ";
+        $query .= "WHERE id = {$safe_subject_id} ";
+        $query .= "LIMIT 1";
+        $page_set = mysqli_query($connection, $query);
+        confirm_query($subject_set);
+        if($subject = mysqli_fetch_assoc($subject_set)){
+            return $subject;
+        } else {
+            return null;
+        }
     }
 
 
