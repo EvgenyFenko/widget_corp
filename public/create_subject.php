493538ebc315fc1fcebc83ca5153aedde542b244
Evgeny Fenko <fenko.ievgenii@gmail.com>
2019-05-27 18:37:13 +0300

    end of lesson 109

--- public/create_subject.php
+++ public/create_subject.php
@@ -1,3 +1,4 @@
+<?php require_once("../includes/session.php"); ?>
 <?php require_once("../includes/db_connection.php"); ?>
 <?php require_once("../includes/functions.php"); ?>
 
@@ -18,11 +19,11 @@
         var_dump($result);
 
         if($result) {
-
+        $_SESSION["message"] = "Subject created.";
             redirect_to("manage_content.php");
 
         } else {
-
+            $_SESSION["message"] = "Subject creation failed";
             redirect_to("new_subject.php");
 
         }
