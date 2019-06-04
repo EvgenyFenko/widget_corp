a48ae210cb5de26755460130625df3629dbf72da
Evgeny Fenko <fenko.ievgenii@gmail.com>
2019-06-04 20:16:53 +0300

    started creation edit_admin page

--- public/edit_admin.php
+++ public/edit_admin.php
@@ -1,1 +1,5 @@
-<?php
+<?php require_once("../includes/session.php"); ?>
+<?php require_once("../includes/db_connection.php"); ?>
+<?php require_once("../includes/functions.php"); ?>
+
+<?php var_dump($_GET);
