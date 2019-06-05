9b5258f324d77198ab8d3831496aae906a727b8b
Evgeny Fenko <fenko.ievgenii@gmail.com>
2019-06-05 19:47:29 +0300

    done page edit_admin

--- public/edit_admin.php
+++ public/edit_admin.php
@@ -4,12 +4,30 @@
 <?php require_once("../includes/validation_functions.php"); ?>
 
 <?php
-
     if(isset($_POST['submit'])) {
-?>
+        $required_fields = array("username", "hashed_password");
+        validate_presence($required_fields);
 
+        if(empty($errors)) {
+            $id = mysql_prep($_GET["id"]);
+            $username = mysql_prep($_POST["username"]);
+            $password = mysql_prep($_POST["hashed_password"]);
 
-<?php
+            $query = "UPDATE admins SET ";
+            $query .= "username = '{$username}', ";
+            $query .= "hashed_password = '{$password}' ";
+            $query .= "WHERE id = {$id} ";
+            $query .= "LIMIT 1";
+
+            $result = mysqli_query($connection, $query);
+
+            if ($result && mysqli_affected_rows($connection) >= 0) {
+                $_SESSION["message"] = "Admin updated.";
+                redirect_to("manage_admins.php");
+            } else {
+                $message = "Admin update failed";
+            }
+        }
     } else {
 ?>
         <?php $layout_context = "admin"; ?>
@@ -24,9 +42,9 @@
                 }
                 ?>
                 <?php echo form_errors($errors); ?>
-                <?php $current_admin = find_admin_by_id($_GET['id']) ?>
+                <?php $current_admin = find_admin_by_id($_GET['id']); ?>
                 <h2>Edit Admin <?php echo htmlentities($current_admin['username']) ?></h2>
-                <form action="edit_admin.php?admin=<?php echo urlencode($current_admin["id"]); ?>" method="post">
+                <form action="edit_admin.php?id=<?php echo urlencode($current_admin["id"]); ?>" method="post">
                     <p>Username:
                         <input type="text" name="username" value="<?php echo htmlentities($current_admin["username"]); ?>" />
                     </p>
@@ -39,7 +57,6 @@
                 <a href="manage_admins.php">Cancel</a>
             </div>
         </div>
-
 <?php
     }
 ?>
